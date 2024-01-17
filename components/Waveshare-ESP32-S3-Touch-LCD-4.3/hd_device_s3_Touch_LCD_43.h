3c91cec87cb123d3b18f076edb217978c63bd797
Allen Bauer <kylix.rd@gmail.com>
2024-01-16 19:11:10 -0800

    Initial commit for Waveshare 4.3 inch LCD

---
+++ components/Waveshare-ESP32-S3-Touch-LCD-4.3/hd_device_s3_Touch_LCD_43.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+#include "ESP32_Display_Panel/ESP_Panel.h"
+#include "lvgl.h"
+
+LV_IMG_DECLARE(bg_480x320);
+
+namespace esphome {
+namespace hd_device {
+
+class HaDeckDevice : public Component
+{
+public:
+    void setup() override;
+    void loop() override;
+    float get_setup_priority() const override;
+private:
+    unsigned long time_ = 0;
+};
+
+}  // namespace hd_device
+}  // namespace esphome
